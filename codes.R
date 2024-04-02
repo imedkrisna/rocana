bf7995b85565be82f6f42d5131d2324a0f562175
Krisna Gupta <44009808+imedkrisna@users.noreply.github.com>
2024-04-02 22:21:52 +0700

    wew

--- codes.R
+++ codes.R
@@ -2,6 +2,7 @@ setwd('C:/github/rocana')
 
 library(WDI)
 library(tidyverse)
+library(scales)
 
 ## Global manufacturing exports
 
@@ -12,7 +13,7 @@ indi<-c(            # membuat dictionary
 )
 
 dat<-WDI(           # Menarik data World Bank
-  country=c("WLD","IDN","CHN","VNM","MYS","THA"),
+  country=c("WLD","IDN","CHN","VNM","MYS","THA","IND","JPN","KOR"),
   indicator=indi,
   start=2002,end=2022
 )
@@ -29,8 +30,18 @@ dat2$manxw<-dat2$manex*dat2$manexp/100
 dat2<-dat2|>select(year,manxw)
 
 dat3<-inner_join(dat,dat2,by="year")
+dat3$mx<-dat3$manx/dat3$manxw*100
 
 dat |>
   filter(country!="World" & country!="China") |>
-  ggplot(aes(x=year,y=manx,color=country))+geom_line(linewidth=1.1)+theme_classic()
+  ggplot(aes(x=year,y=manx,color=country))+geom_line(linewidth=1.1)+
+  scale_y_continuous(labels = label_number())+theme_classic()
+
+dat3 |>
+  filter(country!="World"& country!="China") |>
+  ggplot(aes(x=year,y=mx,color=country))+geom_line(linewidth=1.1)+
+  labs(x="",y="% world manufactures exports",
+       caption="sumber: World Development Indicators")+theme_classic()
+
+ggsave("fig/exports.png")
 
